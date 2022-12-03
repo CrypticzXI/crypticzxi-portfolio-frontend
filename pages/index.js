fdb24ebc3411beaadf6c51b17734cb902603d713
CrypticzXI <CrypticzXI@gmail.com>
2022-12-03 10:46:51 +0000

    Added og:meta tags

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ const home = ({
             <link rel="shortcut icon" href="https://cdn.sanity.io/images/4i4gqgxn/production/27eda8e5bb24a2d31be80a8062077af933ddbb99-1534x1193.png" />
 
             <meta content="CrypticzXI - Portfolio" property="og:title" />
-            <meta id="embed-desc" property="og:description" content={portfolios[0].owninguser.aboutme.desc}></meta>
+            <meta id="embed-desc" property="og:description" content="Never live life without a hope. Hope is a tree of life."></meta>
           </Head>
 
           <div className="fixed top-0 w-full h-16 bg-clip-padding backdrop-blur-xl bg-opacity-50 bg-Background z-[500] flex items-center xl:px-40 lg:px-32 md:px-20 px-8 transition-all">
