ac8d1033413f76fb22ceec2378c8a25ecc3c1f2e
CrypticzXI <69183820+CrypticzXI@users.noreply.github.com>
2023-03-06 09:46:34 +0000

    Fix Blogs

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -32,10 +32,10 @@ const blogpost = ({
         <meta content={"CrypticzXI - Blog Post"} property="og:title" />
         <meta id="embed-desc" property="og:description" content={title + " - " + currentdate}></meta>
         <meta name="theme-color" content="#198794"></meta>
-        {Image0 ? <meta property="og:image" content={Image0}></meta>: null}
+        {Image0 ? <meta property="og:image" content={Image0}></meta> : null}
         <meta name="twitter:card" content="summary_large_image"></meta>
-        <meta id="image-src" name="twitter:image:src" content={Image0}></meta>
-        <meta id="discord" name="twitter:image" content={Image0}></meta>
+        {Image0 ? <meta id="image-src" name="twitter:image:src" content={Image0}></meta> : null}
+        {Image0 ? <meta id="discord" name="twitter:image" content={Image0}></meta> : null}
       </Head>
 
       <div className="fixed top-0 w-full h-16 bg-clip-padding backdrop-blur-xl bg-opacity-50 bg-Background z-[500] flex items-center xl:px-40 lg:px-32 md:px-20 px-8 transition-all">
